2c350e26880c2ce2ed418a51f622e2d0d089b387
Paul Vega <paul.vega@tranzact.net>
2024-06-25 14:00:54 -0500

    chore: update release yml

--- release.config.js
+++ release.config.js
@@ -1,20 +1,24 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-    branches: ['main'],
-    repositoryUrl: 'https://github.com/pvega95/semantic-release-app',
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json'],
-          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        }
-      ]
-    ]
-  }
-  
+export default {
+  branches: ["main"],
+  repositoryUrl: "https://github.com/pvega95/semantic-release-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "CHANGELOG.md",
+          "package.json",
+          "package-lock.json",
+          "npm-shrinkwrap.json",
+        ],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
