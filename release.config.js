4ef6ffcc157803b9600ccdddb200f872314d13d1
Paul Vega <paul.vega@tranzact.net>
2024-06-25 13:23:19 -0500

    chore: initial project setup

---
+++ release.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    branches: ['main'],
+    repositoryUrl: 'https://github.com/pvega95/semantic-release-app',
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/github',
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }
+      ]
+    ]
+  }
+  
