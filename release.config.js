789bc08206bcc5323da4534dd899e4ea74e26a90
Paul Vega <paul.vega@tranzact.net>
2024-06-26 11:06:04 -0500

    chore: setup pre-release configuration

--- release.config.js
+++ release.config.js
@@ -1,24 +1,24 @@
 export default {
-  branches: ["main"],
-  repositoryUrl: "https://github.com/pvega95/semantic-release-app",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
-    "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: [
-          "CHANGELOG.md",
-          "package.json",
-          "package-lock.json",
-          "npm-shrinkwrap.json",
-        ],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+  branches: [
+    'main',
+    { name: 'develop', prerelease: true }
   ],
-};
+  repositoryUrl: 'https://github.com/your-username/your-repo-name',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        // eslint-disable-next-line no-undef
+        assets: process.env.BRANCH_NAME === 'develop'
+          ? ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json']
+          : ['CHANGELOG.md', 'README.md', 'docs/**', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  ]
+}
